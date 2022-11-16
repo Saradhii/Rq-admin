ff7d8500b46cadffe3f9e5f67e4d60c9277e4dd3
Saradhi <saradhi0001069@gmail.com>
2022-11-16 19:50:15 +0530

    new updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
-import RequestQuote2 from "./components/RequestQuote2";
 import { Routes, Route } from "react-router-dom";
 import Admin from "./components/Admin";
 
 function App() {
   return (
     <>
-      {/* <RequestQuote2 /> */}
       <Routes>
-        <Route path="/requestquote" element={<RequestQuote2 />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/" element={<Admin />} />
       </Routes>
     </>
   );
